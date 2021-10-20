f8038b9608824f1c8b08e5eb8bf62ffd3538e67b
RakibulIslam <rakibulislamsofficial@gmail.com>
2021-10-20 12:39:39 +0600

    small fixed

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -11,7 +11,7 @@ const Banner = () => {
                 <div className="text-gray-800 space-y-4">
                     <h1 className="text-5xl sm:text-3xl xs:text-3xl font-bold">Get the smile you've always wanted —</h1>
                     <p className=" text-2xl sm:text-xl xs:text-xl">A full range of cosmetic dentistry treatment!</p>
-                    <Link className=" block" to="/appointment">
+                    <Link className="inline-block" to="/appointment">
                         <button className="px-6 py-2 bg-blue-900 text-white font-semibold shadow-none hover:opacity-90 hover:shadow-xl transition-all duration-300">Book an appointment</button>
                     </Link>
                 </div>
